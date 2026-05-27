7532f2350f6e371196546e04f04eb958e7f4f5a4
Marcio <marcio_videira@hotmail.com>
2026-05-27 14:20:55 -0300

    Finaliza o endpoint de status para avaliação de saúde e outras informações do servidor

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,11 +1,18 @@
-import database from "/infra/database.js";
+import database from '/infra/database.js';
 
 async function status(req, res) {
-  const result = await database.query("SELECT 1 + 1 as soma;");
-  console.log(result.rows);
-  res
-    .status(200)
-    .json({ status: "alunos do curso.dev são pessoas acima da média" });
+  const updatedAt = new Date().toISOString();
+
+  const databaseVersionResult = await database.query('SHOW server_version;');
+  const databaseMaxConnectionsResult = await database.query(
+    'SHOW max_connections;',
+  );
+
+  res.status(200).json({
+    updated_at: updatedAt,
+    db_version: databaseVersionResult.rows[0].server_version,
+    max_connections: databaseMaxConnectionsResult.rows[0].max_connections,
+  });
 }
 
 export default status;
--- tests/unit/integration/api/v1/status/get.test.js
+++ tests/unit/integration/api/v1/status/get.test.js
@@ -1,4 +1,20 @@
-test("GET to /api/v1/status should return 200", async () => {
+test("Get to /api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+
+  expect(responseBody.updated_at).toBeDefined();
+
+  const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
+  expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
+  console.log(responseBody.updated_at);
+
+  const version = responseBody.db_version;
+  expect(version).toEqual("16.13");
+  console.log(version);
+
+  const maxConnections = responseBody.max_connections;
+  expect(maxConnections).toEqual("100");
+  console.log(maxConnections);
 });
